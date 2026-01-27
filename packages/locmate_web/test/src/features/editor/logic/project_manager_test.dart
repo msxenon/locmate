2e0924d31a5cae841e97f71d864e46c2208944c3
Modi <10958931+msxenon@users.noreply.github.com>
2026-01-27 07:38:30 +0100

    fix tests

--- packages/locmate_web/test/src/features/editor/logic/project_manager_test.dart
+++ packages/locmate_web/test/src/features/editor/logic/project_manager_test.dart
@@ -1,105 +1,102 @@
-// import 'dart:convert';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:locmate/locmate.dart';
+import 'package:locmate_web/src/data/datasources/project_datasource.dart';
+import 'package:locmate_web/src/data/models/key_format.dart';
+import 'package:locmate_web/src/data/models/l10n_yaml_model.dart';
+import 'package:locmate_web/src/data/models/project_response.dart';
+import 'package:locmate_web/src/data/repositories/project_repository.dart';
+import 'package:locmate_web/src/features/editor/logic/locmate_settings_model.dart';
+import 'package:locmate_web/src/features/editor/logic/project_manager.dart';
+import 'package:mocktail/mocktail.dart';
 
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:locmate/locmate.dart';
-// import 'package:locmate_web/src/data/datasources/project_datasource.dart';
-// import 'package:locmate_web/src/data/models/key_format.dart';
-// import 'package:locmate_web/src/data/models/l10n_yaml_model.dart';
-// import 'package:locmate_web/src/data/models/project_response.dart';
-// import 'package:locmate_web/src/data/repositories/project_repository.dart';
-// import 'package:locmate_web/src/features/editor/logic/locmate_settings_model.dart';
-// import 'package:locmate_web/src/features/editor/logic/project_manager.dart';
-// import 'package:mocktail/mocktail.dart';
+import '../../../../utils/mocks.dart';
 
-// import '../../../../utils/mocks.dart';
+void main() {
+  late ProviderContainer container;
+  late MockProjectDataSource mockProjectDataSource;
+  late MockProjectRepository mockProjectRepository;
 
-// void main() {
-//   late ProviderContainer container;
-//   late MockProjectDataSource mockProjectDataSource;
-//   late MockProjectRepository mockProjectRepository;
+  setUp(() {
+    registerFallbackValue(FileOpContextDelete(path: ''));
+    registerFallbackValue(LocmateSettingsModel(
+      projectName: 'Demo Project',
+      keyFormat: KeyFormat.none,
+    ));
+    registerFallbackValue(L10nYamlModel(
+      arbDir: 'lib/l10n',
+      templateArbFile: 'app_en.arb',
+    ));
+    mockProjectDataSource = MockProjectDataSource();
+    mockProjectRepository = MockProjectRepository();
+    when(() => mockProjectDataSource.getProjectPath())
+        .thenAnswer((_) async => '/fake/path');
+    container = ProviderContainer(
+      overrides: [
+        projectDatasourceProvider.overrideWithValue(mockProjectDataSource),
+        projectRepositoryProvider.overrideWithValue(mockProjectRepository),
+      ],
+    );
+  });
 
-//   setUp(() {
-//     registerFallbackValue(FileOpContextDelete(path: ''));
-//     registerFallbackValue(LocmateSettingsModel(
-//       projectName: 'Demo Project',
-//       keyFormat: KeyFormat.none,
-//     ));
-//     registerFallbackValue(L10nYamlModel(
-//       arbDir: 'lib/l10n',
-//       templateArbFile: 'app_en.arb',
-//     ));
-//     mockProjectDataSource = MockProjectDataSource();
-//     mockProjectRepository = MockProjectRepository();
-//     container = ProviderContainer(
-//       overrides: [
-//         projectDatasourceProvider.overrideWithValue(mockProjectDataSource),
-//         projectRepositoryProvider.overrideWithValue(mockProjectRepository),
-//       ],
-//     );
-//   });
+  tearDown(() {
+    container.dispose();
+  });
 
-//   tearDown(() {
-//     container.dispose();
-//   });
+  test('should return $ProjectEmpty', () async {
+    when(() => mockProjectRepository.getLocmateModel())
+        .thenAnswer((_) async => null);
+    when(() => mockProjectRepository.getL10nModel())
+        .thenAnswer((_) async => null);
+    final s = await container.read(projectManagerProvider.future);
 
-//   test('should return $ProjectEmpty', () async {
-//     when(() => mockProjectDataSource.getProjectPath())
-//         .thenAnswer((_) async => '/fake/path');
-//     when(() => mockProjectRepository.getLocmateModel())
-//         .thenAnswer((_) async => null);
-//     when(() => mockProjectRepository.getL10nModel())
-//         .thenAnswer((_) async => null);
-//     final s = await container.read(projectManagerProvider.future);
+    expect(
+      s,
+      ProjectEmpty(
+        projectPath: '/fake/path',
+      ),
+    );
+  });
 
-//     expect(
-//       s,
-//       ProjectEmpty(
-//         projectPath: '/fake/path',
-//       ),
-//     );
-//   });
+  test('should create new project', () async {
+    when(() => mockProjectRepository.getLocmateModel())
+        .thenAnswer((_) async => null);
+    when(() => mockProjectRepository.getL10nModel())
+        .thenAnswer((_) async => null);
+    when(() => mockProjectRepository.listArbFiles(any()))
+        .thenAnswer((_) async => []);
+    when(() => mockProjectRepository.saveLocmateModel(any()))
+        .thenAnswer((_) async => []);
+    when(() => mockProjectRepository.saveL10nModel(any()))
+        .thenAnswer((_) async => []);
+    when(() => mockProjectDataSource.fileOp(any()))
+        .thenAnswer((_) async => StringOpResponse(response: ''));
+    when(() => mockProjectRepository.getProjectPubspec())
+        .thenAnswer((_) async => null);
+    when(() => mockProjectRepository.saveArbFileContent(any(), any()))
+        .thenAnswer((_) async => {});
+    await container.read(projectManagerProvider.future);
+    await container.read(projectManagerProvider.notifier).createNewProject();
 
-//   test('should create new project', () async {
-//     when(() => mockProjectDataSource.getProjectPath())
-//         .thenAnswer((_) async => '/fake/path');
-//     when(() => mockProjectRepository.getLocmateModel())
-//         .thenAnswer((_) async => null);
-//     when(() => mockProjectRepository.getL10nModel())
-//         .thenAnswer((_) async => null);
-//     when(() => mockProjectRepository.listArbFiles(any()))
-//         .thenAnswer((_) async => []);
-//     when(() => mockProjectRepository.saveLocmateModel(any()))
-//         .thenAnswer((_) async => []);
-//     when(() => mockProjectRepository.saveL10nModel(any()))
-//         .thenAnswer((_) async => []);
-//     when(() => mockProjectDataSource.fileOp(any()))
-//         .thenAnswer((_) async => StringOpResponse(response: ''));
+    verify(() => mockProjectRepository.saveL10nModel(
+          L10nYamlModel(
+            arbDir: 'lib/l10n',
+            templateArbFile: 'app_en.arb',
+          ),
+        )).called(1);
 
-//     await container.read(projectManagerProvider.future);
-//     await container.read(projectManagerProvider.notifier).createNewProject();
-
-//     verify(() => mockProjectRepository.saveL10nModel(
-//           L10nYamlModel(
-//             arbDir: 'lib/l10n',
-//             templateArbFile: 'app_en.arb',
-//           ),
-//         )).called(1);
-
-//     verify(() => mockProjectDataSource.fileOp(
-//           FileOpContextWrite(
-//             path: 'lib/l10n/app_en.arb',
-//             content: jsonEncode({'@@locale': 'en'}),
-//           ),
-//         )).called(1);
-//     verify(
-//       () => mockProjectRepository.saveLocmateModel(
-//         LocmateSettingsModel(
-//           projectName: 'New project',
-//           keyFormat: KeyFormat.camelCase,
-//           localesOrder: ['en'],
-//         ),
-//       ),
-//     ).called(1);
-//   });
-// }
+    verify(() => mockProjectRepository.saveArbFileContent(
+          'lib/l10n/app_en.arb',
+          {'@@locale': 'en'},
+        )).called(1);
+    verify(
+      () => mockProjectRepository.saveLocmateModel(
+        LocmateSettingsModel(
+          projectName: 'New project',
+          keyFormat: KeyFormat.camelCase,
+          localesOrder: ['en'],
+        ),
+      ),
+    ).called(1);
+  });
+}
