13cbc091d09ea34cfafbb49d495f3a72db30e61e
Modi <10958931+msxenon@users.noreply.github.com>
2026-02-23 08:40:49 +0100

    Refactor AppContainer to use StatefulWidget for improved state management; initialize and dispose of ProviderContainer in the state class.

--- packages/locmate_web/lib/src/app_container.dart
+++ packages/locmate_web/lib/src/app_container.dart
@@ -2,15 +2,34 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:locmate_web/src/app_widget.dart';
 
-class AppContainer extends StatelessWidget {
+class AppContainer extends StatefulWidget {
   final ProviderContainer? testContainer;
   const AppContainer({super.key, @visibleForTesting this.testContainer});
 
   @override
+  State<AppContainer> createState() => _AppContainerState();
+}
+
+class _AppContainerState extends State<AppContainer> {
+  late ProviderContainer container;
+  @override
+  void initState() {
+    container = widget.testContainer ?? ProviderContainer();
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return UncontrolledProviderScope(
-      container: testContainer ?? ProviderContainer(),
+      container: container,
       child: MyApp(),
     );
+  }
+
+  @override
+  void dispose() {
+    container.dispose();
+    super.dispose();
   }
 }
